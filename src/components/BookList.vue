b90e2b8a21b0ab7056d39838c0875e3f02cacef4
Patrick Glynn <p.glynn18@outlook.com>
2019-03-25 22:13:37 +0000

    Final changes
    Created a form to add book title and author.
    
    It adds a generic object to the list of object and displays on screen

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -5,17 +5,19 @@
             <ul>
                 <book-item v-for="book in books" :book='book'></book-item>
             </ul>
-
+            <hr>
+            <book-form @addBook='appendBook'></book-form>
         </div>
             
         </template>
         <script>
         import BookItem from './BookItem';
-     
+        import BookForm from './BookForm';
         export default {
             name: 'BookList',
                components: {
-            BookItem
+            BookItem,
+            BookForm
         },
             data() {
                 return {
@@ -26,6 +28,12 @@
                     {title: 'Lord of the Flies', author: 'William Golding'},
                     {title: 'Lord of the Flies', author: 'William Golding'},
                     ]
+                }
+            },
+            methods: {
+                appendBook(bookTitle, bookAuthor){
+
+                    this.books.push({title: bookTitle, author : bookAuthor})
                 }
             }
         }
