ab2464e8bded23d7c2ebea0800b6834188256408
Patrick Glynn <p.glynn18@outlook.com>
2019-03-25 21:32:12 +0000

    Update BookList.vue

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -3,16 +3,20 @@
         <div>
             <h1>{{title}}</h1>
             <ul>
-                <li v-for="book in books">{{book.title}}:{{book.author}}</li>
+                <book-item v-for="book in books" :book='book'></book-item>
             </ul>
 
         </div>
             
         </template>
-        
         <script>
+        import BookItem from './BookItem';
+     
         export default {
             name: 'BookList',
+               components: {
+            BookItem
+        },
             data() {
                 return {
                     title: 'All Books',
@@ -37,12 +41,6 @@
                 padding: 0;
 
           }
-
-        li {
-            display: block;
-            margin: 0 10px;
-            }
-        
         </style>
 
 
